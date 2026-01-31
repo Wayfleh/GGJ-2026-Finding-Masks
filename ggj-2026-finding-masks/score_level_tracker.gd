dc3a21178921e899240652382fc4521985ff29c6
verynoname <verynoname@gmail.com>
2026-01-30 16:33:42 -0800

    Score tracker
    I think this should display a number at the bottom, and has a function to increment and update the score tracker
    score is level, in this case

---
+++ ggj-2026-finding-masks/score_level_tracker.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var score = 0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.text = str(score)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func incrementAndUpdateScore() -> void:
+	score = score + 1
+	self.text = str(score)
