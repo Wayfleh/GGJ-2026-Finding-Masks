39defa9f23fadd9ef44c1760bdd2836e6e90124e
Wayfleh <rafael.caculba04@gmail.com>
2026-01-30 18:04:22 -0800

    mouse clicks on a mask
    signals come from masks to the main function to fill mask cache. Clicking the left mouse button when hovering over a mask will do something. haven't determined it yet.

--- ggj-2026-finding-masks/Scripts/mask.gd
+++ ggj-2026-finding-masks/Scripts/mask.gd
@@ -5,6 +5,7 @@ class_name Mask extends Area2D
 
 static var mask_scene = preload("res://Scenes/mask.tscn")
 
+
 #Creates a new mask
 static func spawn(index: int) -> Mask:
 	var new = mask_scene.instantiate()
