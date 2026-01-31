bcb98d3462f04f8fd3a7c329a42feb162d0f394e
Wayfleh <rafael.caculba04@gmail.com>
2026-01-30 16:29:27 -0800

    placeholder for the masks

---
+++ ggj-2026-finding-masks/Scenes/mask.gd
@@ -0,0 +1,4 @@
+class_name Mask extends Area2D
+
+@onready var mask_index: int = -1
+@onready var sprite := $Sprite2D
---
+++ ggj-2026-finding-masks/Scripts/main.gd
@@ -0,0 +1,5 @@
+class_name Main extends Node2D
+
+#this will spawn masks at random positions around the map
+func mask_manager(num: int, mask_index: int) -> void:
+	pass
