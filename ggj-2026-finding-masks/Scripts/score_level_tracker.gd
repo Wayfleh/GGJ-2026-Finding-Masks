4171f15d4ab6c9ee0df8c4d9302b4ab957e53c91
verynoname <verynoname@gmail.com>
2026-01-31 17:06:52 -0800

    Added and integrated Global Score Tracker
    Score now properly tracks between gameplay and gameover.
    Score now resets when pressing Play from the title screen

--- ggj-2026-finding-masks/Scripts/score_level_tracker.gd
+++ ggj-2026-finding-masks/Scripts/score_level_tracker.gd
@@ -1,9 +1,10 @@
 extends Node
-
-var score = 0
+"""
+Score tracker for the gameplay scene
+"""
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.text = str(score)
+	self.text = str(GlobalScoreTracker.score)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,5 +12,5 @@ func _process(delta: float) -> void:
 	pass
 
 func incrementAndUpdateScore() -> void:
-	score = score + 1
-	self.text = str(score)
+	GlobalScoreTracker.score = GlobalScoreTracker.score + 1
+	self.text = str(GlobalScoreTracker.score)
