55e029be2496d9256368149c006863e82750e519
verynoname <verynoname@gmail.com>
2026-02-01 11:46:42 -0800

    Adjusted UI, Title added

--- ggj-2026-finding-masks/Scripts/game_over.gd
+++ ggj-2026-finding-masks/Scripts/game_over.gd
@@ -12,8 +12,15 @@ func _ready() -> void:
 
 
 func _on_retry_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/TitleScreen.tscn")
+	GlobalScoreTracker.score = 0
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+	
+	
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/TitleScreen.tscn")
