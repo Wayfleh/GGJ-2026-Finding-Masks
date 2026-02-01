3288bda362cc38f878b466d78b5030a70c445ad6
Wayfleh <rafael.caculba04@gmail.com>
2026-01-31 20:45:35 -0800

    high scores

--- ggj-2026-finding-masks/Scripts/game_over.gd
+++ ggj-2026-finding-masks/Scripts/game_over.gd
@@ -1,13 +1,14 @@
 extends Control
 
+@onready var score = $Score
+@onready var high_score = $HighScore
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	GlobalScoreTracker.check_high_score()
+	score.text = str(GlobalScoreTracker.score)
+	high_score.text = str(GlobalScoreTracker.high_score)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 
 func _on_retry_pressed() -> void:
